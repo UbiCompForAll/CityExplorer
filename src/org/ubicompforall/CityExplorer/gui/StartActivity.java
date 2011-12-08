3493de04c2df644403cfd387188449610399927c
satre <satre@idi.ntnu.no>
2011-12-08 11:08:55 +0100

    Flyttet INIT og GPS (midlertidig) tilbake fra CityExplorer.java til StartActivity. Endret alle fovorite to favourite, to match the ce.sqlite db British Style.

--- src/org/ubicompforall/CityExplorer/gui/StartActivity.java
+++ src/org/ubicompforall/CityExplorer/gui/StartActivity.java
@@ -47,17 +47,24 @@ import android.widget.Toast;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.content.Context;
 import android.content.Intent;
 import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 
-public class StartActivity extends Activity implements OnClickListener{
+public class StartActivity extends Activity implements OnClickListener, LocationListener{
+	private static final String C = "CityExplorer";
+
 	//RS-111122, "implements LocationListener{" moved to CityExplorer.java
 
 	/**
 	 * The buttons in this activity.
 	 */
-	private Button buttonPlan, buttonExplore, buttonImport;
+	private int[] STARTBUTTONIDS = new int[]{R.id.startButtonPlan, R.id.startButtonExplore, R.id.startButtonSettings};
+	private String[] STARTBUTTONNAMES = new String[]{"R.id.startButtonPlan", "R.id.startButtonExplore", "R.id.startButtonSettings"};
+	private Button[] STARTBUTTONS = new Button[3];
 
 	/**
 	 * The user's current location.
@@ -70,42 +77,41 @@ public class StartActivity extends Activity implements OnClickListener{
 		setContentView(R.layout.startlayout);
 		Log.d("CityExplorer", "StartActivity~72");
 
-		buttonPlan	 = (Button) findViewById(R.id.startButtonPlan);
-		if (buttonPlan != null){
-			buttonPlan.setOnClickListener(this);
-		}else{
-			Log.d("CityExplorer", "Plan-button was NULL ~77");
-		}
-		buttonExplore = (Button) findViewById(R.id.startButtonExplore);
-		if (buttonExplore != null){
-			buttonExplore.setOnClickListener(this);
-		}else{
-			Log.d("CityExplorer", "Explorer-button was NULL ~83");
-		}
-		buttonImport = (Button) findViewById(R.id.buttonSettings);
-		if (buttonImport != null){
-			buttonImport.setOnClickListener(this);
-		}else{
-			Log.d("CityExplorer", "Import-button was NULL ~90");
-		}
+		setStartButtons();
 
-		//initGPS(); //RS-111122 Moved to CityExplorer.java Application (Common for all activites)
+		initGPS(); //RS-111208 Move to CityExplorer.java Application (Common for all activites)
 		//startActivity(new Intent(StartActivity.this, ImportActivity.class));
 	}//onCreate
 
+	public void setStartButtons() {
+		for(int b=0; b<STARTBUTTONS.length; b++){
+			//Log.d(C, "b is "+b);
+			STARTBUTTONS[b] = (Button) findViewById(STARTBUTTONIDS[b]);
+			if (STARTBUTTONS[b] != null){
+				STARTBUTTONS[b].setOnClickListener(this);
+			}else{
+				Log.d("CityExplorer", STARTBUTTONNAMES[b]+" - button was NULL ~92");
+			}//if button not found
+		}//for each startButton
+	}//setStartButtons
+
+	public int[] getStartButtons() {
+		return STARTBUTTONIDS;
+	}
+
 	@Override
 	public void onClick(View v) {
-		if (v == buttonPlan){
+		Log.d(C, "Clicked: "+v);
+		if (v.getId() == R.id.startButtonPlan){
 			startActivity(new Intent(StartActivity.this, PlanActivity.class));
 
-		}else if (v == buttonImport){
+		}else if (v.getId() == R.id.startButtonSettings){
 			startActivity(new Intent(StartActivity.this, ImportActivity.class));
 			//DEBUG: Remove importbutton from startlayoutview
 			RelativeLayout sv = (RelativeLayout) findViewById(R.id.startView);
 			sv.removeView(v);
 
-
-		}else if (v == buttonExplore){
+		}else if (v.getId() == R.id.startButtonExplore){
 			if(userLocation != null){
 				Intent showInMap = new Intent(StartActivity.this, MapsActivity.class);
 
@@ -136,7 +142,8 @@ public class StartActivity extends Activity implements OnClickListener{
 				Log.d("CityExplorer", " did you forget to activate GPS??! ");
 				Log.d("CityExplorer", " TODO: Proceede with lastknown location (GSM/WiFi/GPS) from preferences");
 			}//if userLocation, else improvise!
-
+		}else{
+			Log.d(C, "Unknown button clicked: "+v);
 		}//if v== button-Plan|Explore|Import
 	}//onClick
 
@@ -144,6 +151,7 @@ public class StartActivity extends Activity implements OnClickListener{
 	/* RS-111122: Moved to CityExplorer.java common Application settings */
 	/**
 	 * Initializes the GPS on the device.
+	 * */
 	void initGPS()
 	{
 		// Acquire a reference to the system Location Manager
@@ -156,7 +164,7 @@ public class StartActivity extends Activity implements OnClickListener{
 		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
 	}
 
-	 Moved to CityExplorer.java common Application settings
+	//Moved to CityExplorer.java common Application settings
 	@Override
 	public void onLocationChanged(Location location) {
 		this.userLocation = location;
@@ -176,6 +184,27 @@ public class StartActivity extends Activity implements OnClickListener{
 	public void onStatusChanged(String provider, int status, Bundle extras) {
 		// TODO Auto-generated method stub
 	}
-	*/
 }//class
 
+
+/*
+Old Student Code:
+buttonPlan	 = (Button) findViewById(R.id.startButtonPlan);
+if (buttonPlan != null){
+	buttonPlan.setOnClickListener(this);
+}else{
+	Log.d("CityExplorer", "Plan-button was NULL ~77");
+}
+buttonExplore = (Button) findViewById(R.id.startButtonExplore);
+if (buttonExplore != null){
+	buttonExplore.setOnClickListener(this);
+}else{
+	Log.d("CityExplorer", "Explorer-button was NULL ~83");
+}
+buttonSettings = (Button) findViewById(R.id.startButtonSettings);
+if (buttonSettings != null){
+	buttonSettings.setOnClickListener(this);
+}else{
+	Log.d("CityExplorer", "Import-button was NULL ~90");
+}
+*/
